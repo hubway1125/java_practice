b39f7746283a532b6396afc466534a017983aed0
Hubway <davidhoho05@gmail.com>
2020-05-09 15:30:32 +0800

    add java

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String[] args) {
+        int intMaxVal = 0; //變數 駝峰式命名
+        final int ALLCAPITAL = 100; //常數 命名全大寫
+
+        intMaxVal = 1;
+        System.out.println("intMaxVal: " + intMaxVal);
+        intMaxVal = 2;
+        System.out.println("intMaxVal: " + intMaxVal);
+        intMaxVal = 3;
+        System.out.println("intMaxVal: " + intMaxVal);
+        intMaxVal = 4;
+        System.out.println("intMaxVal: " + intMaxVal);
+        intMaxVal = 5;
+        System.out.println("intMaxVal: " + intMaxVal);
+        System.out.println("ALLCAPITAL: " + ALLCAPITAL);
+
+
+//        System.out.println("Practice makes perfect!");
+    }
+
+
+
+}
