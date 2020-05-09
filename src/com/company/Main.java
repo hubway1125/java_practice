4db291580f7d5498c4ebb39d17f59382df9c978b
Hubway <davidhoho05@gmail.com>
2020-05-09 15:34:16 +0800

    correcting constant name to a proper coding style

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         int intMaxVal = 0; //變數 駝峰式命名
-        final int ALLCAPITAL = 100; //常數 命名全大寫
+        final int ALL_CAPITAL = 100; //常數 命名全大寫
 
         intMaxVal = 1;
         System.out.println("intMaxVal: " + intMaxVal);
